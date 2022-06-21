f5d374f687c61878f307f2faba6b5e3308b64868
Felipe Oliveira <felipeoes@usp.br>
2022-06-20 23:04:10 -0300

    download view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
 function App() {
   const [selectedFile, setSelectedFile] = useState();
+  const [downloadFile, setDownloadFile] = useState("");
   const [isFilePicked, setIsFilePicked] = useState(false);
 
   const changeHandler = (event) => {
@@ -30,6 +36,25 @@ function App() {
       });
   };
 
+  async function handleDownloadZipFile() {
+    const response = await api.get(`/download/?file_name=${downloadFile}`, {
+      responseType: "arraybuffer",
+    });
+
+    if (response.status === 200 && response.data) {
+      let blob = new Blob([response.data], { type: "application/zip" });
+      const downloadUrl = URL.createObjectURL(blob);
+      let link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = `arquivos_${downloadFile}`;
+      document.body.appendChild(link);
+      link.click();
+
+      return true;
+    }
+
+  }
+
   return (
     <div>
       <input type="file" name="file" onChange={changeHandler} />
@@ -48,6 +73,17 @@ function App() {
       )}
       <div>
         <button onClick={handleSubmission}>Submit</button>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          name="file_name"
+          onChange={(event) => {
+            setDownloadFile(event.target.value);
+          }}
+        />
+        <button onClick={handleDownloadZipFile}>Download</button>{" "}
       </div>
     </div>
   );
